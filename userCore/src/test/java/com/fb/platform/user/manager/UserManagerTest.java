3069039ccf6ae396b1e152966b2cc2a2764f27f7
Vinayak Karnataki <karnataki.v@gmail.com>
2012-03-07 11:52:06 +0530

    More login and logout tests. WIP

--- userCore/src/test/java/com/fb/platform/user/manager/UserManagerTest.java
+++ userCore/src/test/java/com/fb/platform/user/manager/UserManagerTest.java
@@ -13,6 +13,9 @@ import com.fb.platform.user.manager.interfaces.UserManager;
 import com.fb.platform.user.manager.model.auth.LoginRequest;
 import com.fb.platform.user.manager.model.auth.LoginResponse;
 import com.fb.platform.user.manager.model.auth.LoginStatusEnum;
+import com.fb.platform.user.manager.model.auth.LogoutRequest;
+import com.fb.platform.user.manager.model.auth.LogoutResponse;
+import com.fb.platform.user.manager.model.auth.LogoutStatusEnum;
 
 /**
  * @author vinayak
@@ -20,8 +23,8 @@ import com.fb.platform.user.manager.model.auth.LoginStatusEnum;
  */
 public class UserManagerTest extends BaseTestCase {
 
-	/*@Autowired
-	private UserManager userManager = null;*/
+	@Autowired
+	private UserManager userManager = null;
 
 	@Test
 	public void testLoginWithEmail() {
@@ -29,11 +32,79 @@ public class UserManagerTest extends BaseTestCase {
 		request.setUsername("jasvipul@gmail.com");
 		request.setPassword("testpass");
 
-		/*LoginResponse response = userManager.login(request);
+		LoginResponse response = userManager.login(request);
 
 		assertNotNull(response);
 		assertEquals(LoginStatusEnum.LOGIN_SUCCESS, response.getLoginStatus());
 		assertNotNull(response.getSessionToken());
-		assertEquals(1, response.getUserId().intValue());*/
+		System.out.println("SessionToken is : " + response.getSessionToken());
+		assertEquals(1, response.getUserId().intValue());
 	}
+
+	@Test
+	public void testLoginInvalidPassword() {
+		LoginRequest request = new LoginRequest();
+		request.setUsername("jasvipul@gmail.com");
+		request.setPassword("invalid");
+
+		LoginResponse response = userManager.login(request);
+
+		assertNotNull(response);
+		assertEquals(LoginStatusEnum.INVALID_USERNAME_PASSWORD, response.getLoginStatus());
+		assertNull(response.getSessionToken());
+		assertNull(response.getUserId());
+		
+	}
+
+	@Test
+	public void testLoginWithPhone() {
+		LoginRequest request = new LoginRequest();
+		request.setUsername("9326164025");
+		request.setPassword("testpass");
+
+		LoginResponse response = userManager.login(request);
+
+		assertNotNull(response);
+		assertEquals(LoginStatusEnum.LOGIN_SUCCESS, response.getLoginStatus());
+		assertNotNull(response.getSessionToken());
+		System.out.println("SessionToken is : " + response.getSessionToken());
+		assertEquals(1, response.getUserId().intValue());
+	}
+
+	@Test
+	public void testLoginWithSecondPhone() {
+		LoginRequest request = new LoginRequest();
+		request.setUsername("9870587074");
+		request.setPassword("testpass");
+
+		LoginResponse response = userManager.login(request);
+
+		assertNotNull(response);
+		assertEquals(LoginStatusEnum.LOGIN_SUCCESS, response.getLoginStatus());
+		assertNotNull(response.getSessionToken());
+		System.out.println("SessionToken is : " + response.getSessionToken());
+		assertEquals(2, response.getUserId().intValue());
+	}
+
+	@Test
+	public void testLogout() {
+		LoginRequest request = new LoginRequest();
+		request.setUsername("9326164025");
+		request.setPassword("testpass");
+
+		LoginResponse response = userManager.login(request);
+
+		assertNotNull(response);
+		assertEquals(LoginStatusEnum.LOGIN_SUCCESS, response.getLoginStatus());
+		assertNotNull(response.getSessionToken());
+
+		/*LogoutRequest logoutRequest = new LogoutRequest();
+		logoutRequest.setSessionToken(response.getSessionToken());
+
+		LogoutResponse logoutResponse = userManager.logout(logoutRequest);
+
+		assertNotNull(logoutResponse);
+		assertEquals(LogoutStatusEnum.LOGOUT_SUCCESS, logoutResponse.getStatus());*/
+	}
+
 }
