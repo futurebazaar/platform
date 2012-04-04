fe1d95a75b55066f7705170f8cbde9a5adcef0e6
kislaya <Kumar.Kislay@futuregroup.in>
2012-04-04 19:56:18 +0530

    Only generate next key

--- testProject/src/main/java/com/fb/platform/test/util/CryptoKeyGenerationUtil.java
+++ testProject/src/main/java/com/fb/platform/test/util/CryptoKeyGenerationUtil.java
@@ -12,17 +12,6 @@ import org.apache.commons.lang.RandomStringUtils;
 public class CryptoKeyGenerationUtil {
 
 	public static void main(String[] args) {
-		for (int i=0 ;i <3 ; i++ ){
-			if (i==0){
-					System.out.println("Random Crypto Key Present:: " + RandomStringUtils.random(48, true, true).toUpperCase());
-			}
-			if(i==1){
-					System.out.println("Random Crypto Key Previous:: " + RandomStringUtils.random(48, true, true).toUpperCase());
-			}
-			if(i==2){
-					System.out.println("Random Crypto Key Next:: " + RandomStringUtils.random(48, true, true).toUpperCase());
-			}			
-			
-		}
+		System.out.println("Random Crypto Key Next:: " + RandomStringUtils.random(48, true, true).toUpperCase());
 	}
 }
