0bac15ff33629e8ca9ddb1c6c7500afdf369dec4
neha <nehaga@neha-Vostro-3550.(none)>
2012-04-07 15:38:02 +0530

    log4j.xml deleted

--- promotionCore/src/main/java/com/fb/platform/promotion/dao/impl/CouponDaoJdbcImpl.java
+++ promotionCore/src/main/java/com/fb/platform/promotion/dao/impl/CouponDaoJdbcImpl.java
@@ -159,6 +159,7 @@ public class CouponDaoJdbcImpl implements CouponDao {
 			globalCouponUses = jdbcTemplate.queryForObject(LOAD_GLOBAL_COUPON_USES_QUERY, new Object [] {couponId}, new GlobalCouponUsesMapper());
 		} catch (IncorrectResultSizeDataAccessException e) {
 			//no global uses set, that means this is first time use of this promotion
+			log.warn("No global uses set for coupon id " + couponId + " , that means this is first time use of this promotion");
 		}
 		return globalCouponUses;
 	}
@@ -172,6 +173,7 @@ public class CouponDaoJdbcImpl implements CouponDao {
 		try {
 			userCouponUses = jdbcTemplate.queryForObject(LOAD_USER_COUPON_USES_QUERY, new Object[] {couponId, userId}, new UserCouponUsesMapper());
 		} catch (IncorrectResultSizeDataAccessException e) {
+			log.warn("No user uses set for coupon id " + couponId + " , that means this is first time use of this promotion");
 			//no user uses set, that means this is first time use of this promotion
 		}
 		return userCouponUses;
@@ -235,6 +237,7 @@ public class CouponDaoJdbcImpl implements CouponDao {
 				}
 			}, userUsesKeyHolder);
 		} catch (DuplicateKeyException e) {
+			log.error("Duplicate key insertion exception " + e);
 			throw new PlatformException("Duplicate key insertion exception "+e);
 		}
 		
--- promotionCore/src/main/java/com/fb/platform/promotion/dao/impl/PromotionDaoJdbcImpl.java
+++ promotionCore/src/main/java/com/fb/platform/promotion/dao/impl/PromotionDaoJdbcImpl.java
@@ -152,6 +152,7 @@ public class PromotionDaoJdbcImpl implements PromotionDao {
 			globalPromotionUses = jdbcTemplate.queryForObject(LOAD_GLOABL_PROMOTION_USES_QUERY, new Object [] {promotionId}, new GlobalPromotionUsesMapper());
 		} catch (IncorrectResultSizeDataAccessException e) {
 			//no global uses set, that means this is first time use of this promotion
+			log.warn("No global uses set for promotion id " + promotionId + " , that means this is first time use of this promotion");
 		}
 		return globalPromotionUses;
 	}
@@ -169,6 +170,7 @@ public class PromotionDaoJdbcImpl implements PromotionDao {
 		try {
 			userPromotionUses = jdbcTemplate.queryForObject(LOAD_USER_PROMOTION_USES_QUERY, new Object[] {promotionId, userId}, new UserPromotionUsesMapper());
 		} catch (IncorrectResultSizeDataAccessException e) {
+			log.warn("No user uses set for promotion id " + promotionId + " , that means this is first time use of this promotion");
 			//no user uses set, that means this is first time use of this promotion
 		}
 		return userPromotionUses;
@@ -256,6 +258,7 @@ public class PromotionDaoJdbcImpl implements PromotionDao {
 				}
 			}, userUsesKeyHolder);
 		} catch (DuplicateKeyException e) {
+			log.error("Duplicate key insertion exception " + e);
 			throw new PlatformException("Duplicate key insertion exception "+e);
 		}
 	}
