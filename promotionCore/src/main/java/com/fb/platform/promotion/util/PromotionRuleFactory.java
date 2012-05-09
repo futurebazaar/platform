0c615570ac2e4657311cfe8895d1acc058ce098f
Keith Fernandez <Keith.Fernandez@futuregroup.in>
2012-05-09 12:12:45 +0530

    bug 3183 and bug 3226 : Implement White List and Black List Category + Implement Category/Brands/Client as pluggable into every Rule

--- promotionCore/src/main/java/com/fb/platform/promotion/util/PromotionRuleFactory.java
+++ promotionCore/src/main/java/com/fb/platform/promotion/util/PromotionRuleFactory.java
@@ -9,12 +9,10 @@ import com.fb.platform.promotion.dao.OrderDao;
 import com.fb.platform.promotion.rule.PromotionRule;
 import com.fb.platform.promotion.rule.RuleConfiguration;
 import com.fb.platform.promotion.rule.RulesEnum;
-import com.fb.platform.promotion.rule.impl.BuyWorthXGetYPercentOffOnZCategoryRuleImpl;
+import com.fb.platform.promotion.rule.impl.BuyWorthXGetYPercentOffRuleImpl;
+import com.fb.platform.promotion.rule.impl.BuyWorthXGetYRsOffRuleImpl;
 import com.fb.platform.promotion.rule.impl.BuyXBrandGetYRsOffOnZProductRuleImpl;
 import com.fb.platform.promotion.rule.impl.BuyXGetYFreeRuleImpl;
-import com.fb.platform.promotion.rule.impl.BuyWorthXGetYRsOffRuleImpl;
-import com.fb.platform.promotion.rule.impl.BuyWorthXGetYPercentOffRuleImpl;
-import com.fb.platform.promotion.rule.impl.BuyWorthXGetYRsOffOnZCategoryRuleImpl;
 import com.fb.platform.promotion.rule.impl.FirstPurchaseBuyWorthXGetYRsOffRuleImpl;
 
 /**
@@ -51,18 +49,8 @@ public class PromotionRuleFactory {
 			rule.init(ruleConfig);
 			break;
 			
-		case BUY_WORTH_X_GET_Y_RS_OFF_ON_Z_CATEGORY:
-			rule = new BuyWorthXGetYRsOffOnZCategoryRuleImpl();
-			rule.init(ruleConfig);
-			break;
-			
 		case BUY_X_BRAND_GET_Y_RS_OFF_ON_Z_PRODUCT:
 			rule = new BuyXBrandGetYRsOffOnZProductRuleImpl();
-			rule.init(ruleConfig);
-			break;
-			
-		case BUY_WORTH_X_GET_Y_PERCENT_OFF_ON_Z_CATEGORY:
-			rule = new BuyWorthXGetYPercentOffOnZCategoryRuleImpl();
 			rule.init(ruleConfig);
 			break;
 			
