7afad7694ca1be70d77b90547af7ab809b2dc254
Vinayak Karnataki <karnataki.v@gmail.com>
2012-07-02 18:56:56 +0530

    changes

--- sapReceiver/src/main/java/com/fb/platform/sap/client/idoc/platform/impl/DefaultIDocHandler.java
+++ sapReceiver/src/main/java/com/fb/platform/sap/client/idoc/platform/impl/DefaultIDocHandler.java
@@ -3,25 +3,7 @@
  */
 package com.fb.platform.sap.client.idoc.platform.impl;
 
-import generated.ObjectFactory;
-import generated.ZTINLAIDOCTYP;
-import generated.ZTINLASEGDLVR;
-
-import java.io.StringReader;
-import java.util.List;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.fb.commons.PlatformException;
-import com.fb.platform.mom.inventory.to.InventoryTO;
 import com.fb.platform.mom.manager.MomManager;
-import com.fb.platform.mom.manager.PlatformDestinationEnum;
 import com.fb.platform.sap.client.idoc.platform.PlatformIDocHandler;
 
 /**
