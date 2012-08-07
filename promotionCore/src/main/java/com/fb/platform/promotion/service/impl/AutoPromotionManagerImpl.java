22ca30d22e1fe307e568b98f15a4434578594909
Vinayak Karnataki <karnataki.v@gmail.com>
2012-08-07 21:28:51 +0530

    created the manager method for applying auto promotion

--- promotionCore/src/main/java/com/fb/platform/promotion/service/impl/AutoPromotionManagerImpl.java
+++ promotionCore/src/main/java/com/fb/platform/promotion/service/impl/AutoPromotionManagerImpl.java
@@ -3,6 +3,8 @@
  */
 package com.fb.platform.promotion.service.impl;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +39,19 @@ public class AutoPromotionManagerImpl implements AutoPromotionManager {
 
 	@Override
 	public ApplyAutoPromotionResponse apply(ApplyAutoPromotionRequest request) {
+		try {
+			List<Integer> activeAutoPromotions = promotionService.getActiveAutoPromotions();
+		} catch (Exception e) {
+		}
 		return null;
+	}
+
+	public void setAuthenticationService(AuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
+	}
+
+	public void setPromotionService(PromotionService promotionService) {
+		this.promotionService = promotionService;
 	}
 
 }
