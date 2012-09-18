c98887a973efbcd4d372172c9a04a04e58ca497d
kislaya <Kumar.Kislay@futuregroup.in>
2012-09-18 18:56:28 +0530

    Bug 3195: sms text change and smsTO correction

--- walletCore/src/main/java/com/fb/platform/wallet/util/SmsHelper.java
+++ walletCore/src/main/java/com/fb/platform/wallet/util/SmsHelper.java
@@ -15,9 +15,8 @@ import com.fb.commons.communication.to.SmsTO;
 public class SmsHelper {
 	
 	//Used to load the template in this
-	public static String SMS_TEMPLATE_STR = "Hi! ewallet.receiver,your electronic wallet has been created at Futurebazaar.com." +
-"Use ewallet.password as password to pay using wallet. For help," +
-"call 09222221947";
+	public static String SMS_TEMPLATE_STR = "Your Future Bazaar wallet is now active. " +
+"Use pin: ewallet.password to shop using wallet on futurebazaar.com. Please do not share your pin. For queries, please call 0922-222-1947";
 	
 	public static String EWALLET_PASSWORD_SMS_TEMPLATE_STR = "ewallet.password";
 	
@@ -44,9 +43,9 @@ public class SmsHelper {
 		}
 		
 		
-		//Set message using template		
-		String message = SMS_TEMPLATE_STR.replaceAll(EWALLET_PASSWORD_SMS_TEMPLATE_STR, eWalletPassword)
-				.replaceAll(EWALLET_RECEIVER_SMS_TEMPLATE_STR, receiverName);
+		//Set message using template
+		
+		String message = SMS_TEMPLATE_STR.replaceAll(EWALLET_PASSWORD_SMS_TEMPLATE_STR, eWalletPassword);
 		smsTo.setMessage(message);
 		return smsTo;
 	}
