41765e510296991ee7a2d90247307852c64eb25d
Neha Garani <neha.garani@futuregroup.in>
2012-10-29 15:45:48 +0530

    tinla url key changed

--- invoiceReceiver/src/main/java/com/fb/platform/mom/bigBazaar/invoice/receiver/InvoiceMessageReceiver.java
+++ invoiceReceiver/src/main/java/com/fb/platform/mom/bigBazaar/invoice/receiver/InvoiceMessageReceiver.java
@@ -92,10 +92,10 @@ public class InvoiceMessageReceiver implements PlatformMessageReceiver{
 		
 		switch (invoiceTO.getInvoiceType()) {
 		case CREATE:
-			invoiceURL = prop.getProperty("receiver.invoice.create.url");
+			invoiceURL = prop.getProperty("receiver.bigBazaar.invoice.create.url");
 			break;
 		case CANCEL:
-			invoiceURL = prop.getProperty("receiver.invoice.change.url");
+			invoiceURL = prop.getProperty("receiver.bigBazaar.invoice.change.url");
 			break;
 		}
 		
