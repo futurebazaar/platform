bb48b76056a6b67818d42108668e72b7558ff961
Anubhav Jain <anubhav.jain@futuregroup.in>
2012-12-04 10:57:47 +0530

    bug 4719 Added Transaction in post xml to bapi

--- platformScheduler/src/main/java/com/fb/platform/scheduler/service/OrderSchedulerService.java
+++ platformScheduler/src/main/java/com/fb/platform/scheduler/service/OrderSchedulerService.java
@@ -1,11 +1,15 @@
 package com.fb.platform.scheduler.service;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author anubhav
  *
  */
 public interface OrderSchedulerService {
 
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void postXmlsTOBapi();
 
 }
